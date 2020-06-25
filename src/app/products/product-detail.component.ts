6a73f2338099077140455a4df914bcb3f4f147c6
Brezo <brezo.al@gmail.com>
2020-06-25 15:54:52 +0200

    Router techniques: parameters and guards

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -10,9 +11,25 @@ export class ProductDetailComponent implements OnInit {
   productTitle: string = 'Product detail';
   product: IProduct;
 
-  constructor() { }
+  constructor( private activatedRoute: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    let id = +this.activatedRoute.snapshot.paramMap.get('id');
+    this.productTitle += `: ${id}`;
+    this.product = {
+      "productId": 10,
+      "productName": "Video Game Controller",
+      "productCode": "GMG-0042",
+      "releaseDate": "October 15, 2018",
+      "description": "Standard two-button video game controller",
+      "price": 35.95,
+      "starRating": 4.6,
+      "imageUrl": "assets/images/xbox-controller.png"
+    }
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
